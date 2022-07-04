ad4fdd020687423df57e045fa88914679f370e26
Sławomir Czech <slaw.czech@gmail.com>
2022-07-04 20:16:25 +0200

    statics

--- home/views.py
+++ home/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 
 def home_view(request):
     name = 'Alegrosik. Strona startowa'
-    return render(request, 'home/home.html', {"name": name})
+    hello = 'Witaj! Co możemy dla Ciebie zrobić?'
+    return render(request, 'home/home.html', {"name": name, "hello": hello})
 
